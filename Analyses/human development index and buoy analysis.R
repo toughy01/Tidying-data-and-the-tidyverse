1d25b3484438a22670eaae37b01c0c22099b89ff
John Tough <jrt548@york.ac.uk>
2020-12-17 20:10:47 +0000

    Using one pipeline to filter, summarise and plot the hdi data for the top 10 lowest ranking countries on the hdi scale

--- Analyses/human development index and buoy analysis.R
+++ Analyses/human development index and buoy analysis.R
@@ -153,6 +153,32 @@ ggplot()+
 
 
 
-#LET'S TRY AND BUILD A PIPELINE THAT TAKES ALL OF THE ABOVE DATA TIDYING,
+#LET'S TRY AND BUILD A PIPELINE THAT TAKES "HDI_TIDY" DATAFRAME,
 #SUMMARY STATS AND PLOT IN ONE PIPELINE...
+hdi_tidy %>%  
+  filter(!is.na(index)) %>% 
+  group_by(country) %>% 
+  summarise(mean_index = mean(index),
+            se_index = sd(index)/sqrt(length(index))) %>% 
+  filter(rank(mean_index) < 11) %>% 
+  ggplot() +
+  geom_point(aes(x = country,
+                 y = mean_index)) +
+  geom_errorbar(aes(x = country,
+                    ymin = mean_index - se_index,
+                    ymax = mean_index + se_index)) +
+  scale_y_continuous(limits = c(0, 0.5),
+                     expand = c(0, 0),
+                     name = "HDI") +
+  scale_x_discrete(expand = c(0, 0),
+                   name = "") +
+  theme_classic() +
+  coord_flip()
 
+           
+  
+  
+
+
+
+         
